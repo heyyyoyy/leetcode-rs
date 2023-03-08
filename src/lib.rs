60813a3c82dd05276657dd5858913ca57394f069
Sergey <heyyyoyy@gmail.com>
2023-03-08 14:58:43 +0300

    125. Valid Palindrome

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@
 
 mod s1_two_sum;
 mod s20_valid_parentheses;
+mod s125_valid_palindrome;
---
+++ src/s125_valid_palindrome.rs
@@ -0,0 +1,27 @@
+
+struct Solution;
+
+
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let left = s.chars()
+            .filter(|ch| ch.is_alphanumeric())
+            .map(|ch| ch.to_ascii_lowercase());
+        let right = left.clone().rev();
+        left.zip(right).all(|(x, y)| x == y)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid() {
+        assert_eq!(Solution::is_palindrome("A man, a plan, a canal: Panama".to_owned()), true);
+        assert_eq!(Solution::is_palindrome("race a car".to_owned()), false);
+        assert_eq!(Solution::is_palindrome(" ".to_owned()), true);
+        assert_eq!(Solution::is_palindrome("0P".to_owned()), false)
+    }
+}
