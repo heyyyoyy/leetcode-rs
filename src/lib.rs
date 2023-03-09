6e483a635579a4ad5599805501d31aac35eaf8f8
Sergey <heyyyoyy@gmail.com>
2023-03-09 23:12:47 +0300

    121. Best Time to Buy and Sell Stock

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ mod s1_two_sum;
 mod s20_valid_parentheses;
 mod s125_valid_palindrome;
 mod s21_merge_two_sorted_lists;
+mod s121_best_time_stock;
---
+++ src/s121_best_time_stock.rs
@@ -0,0 +1,31 @@
+struct Solution;
+
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut min_buy = prices[0];
+        let mut max_profit = 0;
+        for price in prices {
+            let current_profit = price - min_buy;
+            if current_profit > max_profit {
+                max_profit = current_profit;
+            }
+            if min_buy > price {
+                min_buy = price;
+            }
+        }
+        max_profit
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_profit() {
+        assert_eq!(Solution::max_profit(vec![7,1,5,3,6,4]), 5);
+        assert_eq!(Solution::max_profit(vec![7,6,4,3,1]), 0);
+    }
+}
