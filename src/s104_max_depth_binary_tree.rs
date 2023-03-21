dbfb528ecb7b7f18ba99e0426500860fe093fadf
Sergey <heyyyoyy@gmail.com>
2023-03-21 22:24:54 +0300

    refactoring

--- src/s104_max_depth_binary_tree.rs
+++ src/s104_max_depth_binary_tree.rs
@@ -1,22 +1,9 @@
-// Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
   pub val: i32,
   pub left: Option<Rc<RefCell<TreeNode>>>,
   pub right: Option<Rc<RefCell<TreeNode>>>,
 }
-use std::borrow::Borrow;
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
+
 use std::rc::Rc;
 use std::cell::RefCell;
 
