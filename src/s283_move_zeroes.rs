1936338f968c7a1eaa3cabdad365f0d745a3ab85
Sergey <heyyyoyy@gmail.com>
2023-05-17 22:10:47 +0300

    283. Move Zeroes

--- src/s283_move_zeroes.rs
+++ src/s283_move_zeroes.rs
@@ -17,6 +17,16 @@ impl Solution {
             nums[i] = temp_arr[i];
         }
     }
+
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut idx_non_zero = 0;
+        for i in 0..nums.len() {
+            if nums[i] != 0 {
+                nums.swap(idx_non_zero, i);
+                idx_non_zero += 1;
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -28,5 +38,16 @@ mod tests {
         let mut arr = vec![0, 1, 0, 3, 12];
         Solution::move_zeroes_space_suboptimal(&mut arr);
         assert_eq!(vec![1, 3, 12, 0, 0], arr);
+    }
+
+    #[test]
+    fn test_move_zeroes() {
+        let mut arr = vec![0, 1, 0, 3, 12];
+        Solution::move_zeroes(&mut arr);
+        assert_eq!(vec![1, 3, 12, 0, 0], arr);
+
+        let mut arr = vec![2, 1];
+        Solution::move_zeroes(&mut arr);
+        assert_eq!(vec![2, 1], arr);
     }
 }
